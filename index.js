80580afd1d9b3e8c663173ef0938298c08f37dc7
monteirok <karstenmonteiro@gmail.com>
2023-06-11 18:36:38 -0600

    updated index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const http = require('http');
-const PORT = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
-
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
 // server.js
 const express = require('express');
 const socketIO = require('socket.io');
