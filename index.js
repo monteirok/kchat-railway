20430d4bca5238e6f4aa938adc098fc1d0fcbe0d
monteirok <karstenmonteiro@gmail.com>
2023-06-12 01:01:48 -0600

    updated some files

--- index.js
+++ index.js
@@ -14,24 +14,33 @@ app.use(express.static('public'));
  *   Run on port 3000, and handle connections/chat messages.
 */
 server.listen(3000, () => {
-    process.stdout.write('\033c');
-    console.log('\x1b[2m\x1b[36m=====================\x1b[0m');
-    console.log("  \x1b[37mWelcome to kChat Console!\x1b[0m");
-    console.log('\x1b[2m\x1b[36m=====================\x1b[0m');
-    console.log('\x1b[100m\x1b[32m[SUCCESS]\x1b[0m' + '\x1b[33m Server is running on port ' + '\x1b[4m3000\x1b[0m' + ' \x1b[5m\x1b[35m->\x1b[0m' + '\x1b[2m\x1b[35m http://localhost:3000 \x1b[0m');
-    console.log('\x1b[2m\x1b[36m-_-_-_-_-_-_-_-_-_-_-_');
-    console.log();
+  // clear the console
+  console.clear();
+
+  console.log('\x1b[2m\x1b[36m=====================\x1b[0m');
+  console.log("  \x1b[37mWelcome to kChat Console!\x1b[0m");
+  console.log('\x1b[2m\x1b[36m=====================\x1b[0m');
+  console.log('\x1b[100m\x1b[32m[SUCCESS]\x1b[0m' + '\x1b[33m Server is running on port ' + '\x1b[4m3000\x1b[0m' + ' \x1b[5m\x1b[35m->\x1b[0m' + '\x1b[2m\x1b[35m http://localhost:3000 \x1b[0m');
+  console.log('\x1b[2m\x1b[36m-_-_-_-_-_-_-_-_-_-_-_');
+  console.log();
 });
 
+/**
+ * HANDLE CONNECTIONS
+ */
 io.on('connection', (socket) => {
-    console.log('\x1b[5m\x1b[32m >>\x1b[0m' + '\x1b[2mNew user connected\x1b[0m');
+  // display user connected console msg
+  console.log('\x1b[5m\x1b[32m >>\x1b[0m' + '\x1b[2mNew user connected\x1b[0m');
 
-    socket.on('disconnect', () => {
-        console.log('\x1b[0m\x1b[5m\x1b[31m >>\x1b[0m' + '\x1b[2mUser disconnected\x1b[0m');
-    });
-
-    socket.on('chatMessage', (msg) => {
-        io.emit('chatMessage', msg);
-        console.log('\x1b[5m\x1b[34m [\x1b[0m' + '\x1b[2m\x1b[34mCHAT\x1b[0m' + '\x1b[5m\x1b[34m]\x1b[0m ' + msg);
-    });
+  // disconnection
+  socket.on('disconnect', () => {
+    // display user disconnected console msg
+    console.log('\x1b[0m\x1b[5m\x1b[31m >>\x1b[0m' + '\x1b[2mUser disconnected\x1b[0m');
+  });
+  
+  socket.on('chatMessage', (msg) => {
+    io.emit('chatMessage', msg);
+    // display user chat message to console
+    console.log('\x1b[5m\x1b[34m [\x1b[0m' + '\x1b[2m\x1b[34m' + msg.username + '\x1b[0m' + '\x1b[5m\x1b[34m]\x1b[0m ' + msg.message);
+  });
 });
